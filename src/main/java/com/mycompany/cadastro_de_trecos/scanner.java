9ae284c1a82686892922b2db231750c3c60a658e
Edison Marcos <dev3dison@gmail.com>
2023-05-23 20:38:40 -0300

    2023.05.23

---
+++ src/main/java/com/mycompany/cadastro_de_trecos/scanner.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cadastro_de_trecos;
+
+/**
+ *
+ * @author 16128412023.1
+ */
+class scanner {
+
+    static String next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
