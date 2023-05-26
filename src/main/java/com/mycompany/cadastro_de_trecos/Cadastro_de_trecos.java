93bc5d57af77455faafc3ca7418586ae1a53431d
Edison Marcos <dev3dison@gmail.com>
2023-05-25 21:48:10 -0300

    2023.05.25

--- src/main/java/com/mycompany/cadastro_de_trecos/Cadastro_de_trecos.java
+++ src/main/java/com/mycompany/cadastro_de_trecos/Cadastro_de_trecos.java
@@ -1,15 +1,13 @@
 
 package com.mycompany.cadastro_de_trecos;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.mycompany.cadastro_de_trecos.setup.AppSetup;
+import com.mycompany.cadastro_de_trecos.crud.Read;
 import java.util.Scanner;
+import com.mycompany.cadastro_de_trecos.crud.Delete;
 
-public class Cadastro_de_trecos {
 
-    static Scanner scanner = new Scanner(System.in);
+public class Cadastro_de_trecos extends AppSetup {
 
     public static void main(String[] args) {
         clearScreen();
@@ -18,7 +16,8 @@ public class Cadastro_de_trecos {
 
     // Método que exibe o menu principal.
     public static void mainMenu() {
-        System.out.println("Cadastro de Trecos\n");
+
+        System.out.println(appName + "\n" + appSep);
         System.out.println("Menu:");
         System.out.println("\t[1] Listar todos");
         System.out.println("\t[2] Listar");
@@ -26,7 +25,8 @@ public class Cadastro_de_trecos {
         System.out.println("\t[4] Editar");
         System.out.println("\t[5] Apagar");
         System.out.println("\t[0] Sair");
-        System.out.print("\nOpção: ");
+        System.out.println(appSep);
+        System.out.print("Opção: ");
 
         // Recebe a opção do teclado.
         String option = scanner.next();
@@ -35,16 +35,22 @@ public class Cadastro_de_trecos {
         switch (option) {
             case "0" ->
                 exitProgram();
-            case "1" ->
-                listAll();
-            case "2" ->
-                listOne();
+            case "1" -> {
+                clearScreen();
+                Read.readAll();
+            }
+            case "2" -> {
+                clearScreen();
+                Read.read();
+            }
             case "3" ->
                 newThing();
             case "4" ->
                 editThing();
-            case "5" ->
-                deleteThing();
+            case "5" -> {
+                clearScreen();
+                Delete.delete();
+            }
             default ->
                 reloadMenu();
         }
@@ -58,47 +64,23 @@ public class Cadastro_de_trecos {
         System.exit(0);
     }
 
-    // Lista todos os trecos cadastrados.
-    public static void listAll() {
-
-        try {
-            String sql = "SELECT * FROM things";
-            Connection conn = DbConnection.dbConnect();
-            Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery(sql);
-
-            while (res.next()) {
-                System.out.println(
-                        "ID: " + res.getString("id") + "\n"
-                        + "Nome: " + res.getString("name") + "\n"
-                        + "Descrição: " + res.getString("description") + "\n"
-                );
-            }
-
-            conn.close();
-            stmt.close();
-            res.close();
-
-        } catch (SQLException error) {
-            System.out.println("Oooops! " + error.getMessage());
-            System.exit(0);
-        }
-
-    }
-
-    // Lista um treco específico pelo Id.
-    public static void listOne() {
-    }
-
-    // Cadastra um novo treco.
+    // Cadastra um novo registro.
     public static void newThing() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Item ID: ");
+        String itemID = scan.nextLine();
+        System.out.print("Enter Item price: ");
+        String priceStr = scan.nextLine();
+        double price = Double.valueOf(priceStr);
+        System.out.println("Price of Item " + itemID + " is $" + price);
+        scan.close();
     }
 
-    // Edita um treco pelo Id.
+    // Edita um registro pelo Id.
     public static void editThing() {
     }
 
-    // Apaga um treco pelo Id.
+    // Apaga um registro pelo Id.
     public static void deleteThing() {
     }
 
